f55fd2876c58ab79a8b29ffb7dffba7d0b8aac53
udayanga91 <u.senanayake91@gmail.com>
2020-06-30 22:39:29 +0900

    configure Swagger

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -2,27 +2,44 @@ package guru.springfamework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {//extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket api()  {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("John Thompson", "https://springframework.guru/acout",
+                "john@springframework.guru");
+        return new ApiInfo(
+                "Spring Framework guru"
+                , "Spring Framework 5: Beginers to guru"
+                , "1.0"
+                , "Terms of Service: blath"
+                , contact,
+                "Apache Licence version 2.0"
+                , "https://www.apache.org/licenses/LICENSE-2.0"
+                , new ArrayList<>()
+        );
     }
 
  /*   @Override
